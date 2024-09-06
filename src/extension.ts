48283d6d466cba5c98b4fec9ea2b3bba60b5661e
paultreanor <treanorpaul9@gmail.com>
2024-09-06 11:53:29 +0100

    Change spaces to tabs

--- src/extension.ts
+++ src/extension.ts
@@ -5,44 +5,44 @@ import * as path from 'path';
 let statusBarItem: vscode.StatusBarItem;
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('VSCodesidian extension Running');
+	console.log('VSCodesidian extension Running');
 
-    try {
-        statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-        statusBarItem.text = 'Obsidian Vault';
-        statusBarItem.tooltip = 'VSCodesidian detected open vault';
-        context.subscriptions.push(statusBarItem);
+	try {
+		statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+		statusBarItem.text = 'Obsidian Vault';
+		statusBarItem.tooltip = 'VSCodesidian detected open vault';
+		context.subscriptions.push(statusBarItem);
 
-        detectObsidianVault();
+		detectObsidianVault();
 
-        // Listen for workspace folder changes
-        context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(() => detectObsidianVault()));
-    } catch (error) {
-        console.error('Error activating VSCodesidian:', error);
-    }
+		// Listen for workspace folder changes
+		context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(() => detectObsidianVault()));
+	} catch (error) {
+		console.error('Error activating VSCodesidian:', error);
+	}
 }
 
 function detectObsidianVault() {
-    const workspaceFolders = vscode.workspace.workspaceFolders;
-    if (!workspaceFolders) {
-        statusBarItem.hide();
-        return;
-    }
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (!workspaceFolders) {
+		statusBarItem.hide();
+		return;
+	}
 
-    const workspaceRoot = workspaceFolders[0].uri.fsPath;
-    const obsidianDir = path.join(workspaceRoot, '.obsidian');
+	const workspaceRoot = workspaceFolders[0].uri.fsPath;
+	const obsidianDir = path.join(workspaceRoot, '.obsidian');
 
-    if (fs.existsSync(obsidianDir)) {
-        console.log('Obsidian vault detected');
-        statusBarItem.text = 'Obsidian';
-        statusBarItem.show();
-    } else {
-        statusBarItem.hide();
-    }
+	if (fs.existsSync(obsidianDir)) {
+		console.log('Obsidian vault detected');
+		statusBarItem.text = 'Obsidian';
+		statusBarItem.show();
+	} else {
+		statusBarItem.hide();
+	}
 }
 
 export function deactivate() {
-    if (statusBarItem) {
-        statusBarItem.dispose();
-    }
+	if (statusBarItem) {
+		statusBarItem.dispose();
+	}
 }
